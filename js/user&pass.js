fa04689d869cb0507a10bc4114dc5e828eb1db3f
Canns Coding <CannsCoding@gmail.com>
2023-12-05 18:31:57 +0000

    new pass and user policy

---
+++ js/user&pass.js
@@ -0,0 +1,29 @@
+var username = prompt("What is your Username")
+var Password = prompt("What is your Password")
+
+if (username === "Cann" && Password === "Geek") {
+    alert("Proceed to game")
+}
+else if (username === "Dumb" && Password === "Idiot") {
+    alert("Proceed to game")
+}
+else if (username === "Ty" && Password === "Guy") {
+    alert("Proceed to game")
+}
+else if (username === "Limes" && Password === "Lemons") {
+    alert("Proceed to game")
+}
+else if (username === "CComfront" && Password === "10234677") {
+    alert("Proceed to game")
+}
+else if (username === "Swimmer" && Password === "Boy") {
+    alert("Proceed to game")
+}
+else if (username === "The" && Password === "Kid") {
+    alert("Proceed to game")
+}
+
+else {
+    alert("try again")
+    window.location = "https://cannscoding.github.io"
+}
